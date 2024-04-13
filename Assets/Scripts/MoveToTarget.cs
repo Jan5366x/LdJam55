ee9aa12fa615bdc0b5dd089780937f1764f563d9
Robin Lohe <Robin.Lohe@gmx.de>
2024-04-13 20:05:27 +0200

    Missiles fly to the last enemy position after it was destroyed

--- Assets/Scripts/MoveToTarget.cs
+++ Assets/Scripts/MoveToTarget.cs
@@ -1,34 +1,47 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MoveToTarget : MonoBehaviour
 {
     public Transform target;
     public float moveSpeed;
-    
+
+    private Vector3 lastTargetPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (target is not null)
+        if (target.IsDestroyed() is false)
         {
-            Vector3 dir = transform.position - target.transform.position;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            
-            var targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
-            Vector3 velocity = (targetPos - transform.position).normalized * moveSpeed;
-            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, 5.0f, Time.deltaTime);
-
-            if (Vector3.Distance(transform.position, target.position) <= 0.3f)
-            {
-                target.GetComponent<EntityWithHealth>().ApplyDamage(10);
-                Destroy(gameObject);
-            }
+            lastTargetPosition = target.position;
         }
+
+        Rotate();
+
+        //var targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        var velocity = (lastTargetPosition - transform.position).normalized * moveSpeed;
+        transform.position = Vector3.SmoothDamp(transform.position, lastTargetPosition, ref velocity, 0.5f, Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, lastTargetPosition) <= 0.3f)
+        {
+            if (target.IsDestroyed() is false)
+            {
+                target.GetComponent<EntityWithHealth>().ApplyDamage(10);    
+            }
+            
+            Destroy(gameObject);
+        }
+    }
+
+    private void Rotate()
+    {
+        var dir = transform.position - lastTargetPosition;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
