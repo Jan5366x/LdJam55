46a9d7959e98141708db262631183e59160aa2a3
Jan Schwien <jan5366@gmail.com>
2024-04-14 16:19:32 +0200

    Add sorting order script

---
+++ Assets/Scripts/SortingOrder.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SortingOrder : MonoBehaviour
+{
+    private const int ChildLayerSpace = 100;
+
+    public float groundOffset;
+
+    private int _sortingOrder = 0;
+    private SpriteRenderer _spriteRenderer;
+    private bool _hasOwnRenderer = false;
+ 
+
+    private void Awake()
+    {
+        Prepare();
+        SetPosition();
+    }
+
+    void Update()
+    {
+        SetPosition();
+        
+        var objPos = transform.position;
+        
+        Debug.DrawLine(
+            new Vector3(objPos.x + -0.3F,objPos.y + groundOffset), 
+            new Vector3(objPos.x + 0.3F,objPos.y + groundOffset),
+            _hasOwnRenderer ? Color.yellow : Color.magenta);
+    }
+
+    void SetPosition()
+    {
+        // update sprite renderer if in Editor mode to provide live updates
+        if(!Application.isPlaying)
+            Prepare();
+        
+        _sortingOrder = (int) -((transform.position.y + groundOffset) * ChildLayerSpace);
+        
+        if (_hasOwnRenderer)
+            _spriteRenderer.sortingOrder = _sortingOrder;
+    }
+
+    private void Prepare()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _hasOwnRenderer = _spriteRenderer != null;
+    }
+
+    public int GetSortingOrder()
+    {
+        return _sortingOrder;
+    }
+}
