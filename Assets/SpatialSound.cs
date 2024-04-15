293f649a68d0b648ffed0c717aeb3ced64993d88
Robin Lohe <Robin.Lohe@gmx.de>
2024-04-15 18:06:41 +0200

    Added explosion sound

---
+++ Assets/SpatialSound.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SpatialSound : MonoBehaviour
+{
+    public float minDist = 1;
+    public float maxDist = 6;
+    
+    private Transform _listener;
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _listener = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        var dist = Vector3.Distance(transform.position, _listener.position);
+
+        if (dist < minDist)
+        {
+            _audioSource.volume = 1;
+        }
+        else if (dist > maxDist)
+        {
+            _audioSource.volume = 0;
+        }
+        else
+        {
+            _audioSource.volume = 1 - ((dist - minDist) / (maxDist - minDist));
+        }
+    }
+}
